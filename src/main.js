f28ee7c5f673910098f93ce218688732f5582139
SyedBukhari <syedmubukhari@gmail.com>
2021-04-05 17:55:48 +0100

    Added basic validation according to the Luhn algorithm.

--- src/main.js
+++ src/main.js
@@ -14,10 +14,16 @@ $(document).ready(function () {
             $('.card-form__error').text('Card number is invalid. Please correct it.');
             $('.card-form__submit').prop('disabled', true);
         } else {
-            $('.card-form__error').text('');
-            $('.card-form__submit').prop('disabled', false);
-            
             // Validate with Luhn algorithm only if number entered passes regex validation
+            let luhnCheck = luhnValidate(cardNumber);
+
+            if (luhnCheck) {
+                $('.card-form__error').text('');
+                $('.card-form__submit').prop('disabled', false);
+            } else {
+                $('.card-form__error').text('Card number is invalid. Please correct it.');
+                $('.card-form__submit').prop('disabled', true);
+            }
         }
     });
 
@@ -50,15 +56,47 @@ $(document).ready(function () {
     });
 });
 
-// Validate the credit card number according to the Luhn algorithm
-function luhnValidate() {
+// 
+/**
+ * Runs basic validation on the card number according to the Luhn algorithm
+ * 
+ * @param {number} cardNumber The card number with all formatting removed
+ * @returns {boolean} True is successful else returns false
+ */
+function luhnValidate(cardNumber) {
+    // Convert card number to a string
+    cardNumber = cardNumber + '';
+
+    let digitSum = 0;
+    let maxLength = cardNumber.length - 1;
+
+    // i counts down in order to start at the right-most digit in the string
+    for (let i = maxLength; i >= 0; i--) {
+        var currentDigit = parseInt(cardNumber[i]); // Convert digit back to a number
+
+        if ((maxLength - i) & 1) {
+            let additionValue = currentDigit * 2;
+
+            digitSum += (additionValue < 10) ? additionValue : 1 + (additionValue % 10);
+        } else {
+            digitSum += currentDigit;
+        }
+    }
+
+    let finalValue = digitSum % 10;
+
+    if (finalValue === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 /**
  * Auto-formats the card number into groups of four digits
  * as the user types the number in to make it easier to read
  * 
- * @param {number} cardNumber 
+ * @param {number} cardNumber The card number with all formatting removed
  * @returns {string} Formatted card number for display
  */
 function formatCardNumber(cardNumber) {
